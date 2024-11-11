5125a6545b30d256ce5a2f17218beda477f97329
cezarpm <cezar_pm@hotmail.com>
2024-11-11 13:44:08 -0300

    Revert "feat: adicionado métrica do processamento e stream"
    This reverts commit 061f7ae30edf3ce3b3881e5d546c4aff9625f14b.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import { validateDocument } from "./utils/documentValidator.js";
 import { calculateInstallmentAmount } from "./utils/calculateInstallments.js";
 import fs from "fs";
 import csvParser from "csv-parser";
-import { Writable } from "stream";
 
 const fieldTransformations = {
   documentIsValid: ({ nrCpfCnpj }) => validateDocument(nrCpfCnpj),
@@ -26,87 +25,36 @@ const transformResult = (result) => {
   return result;
 };
 
-const saveToCSVStream = (outputFilename) => {
-  const writableStream = fs.createWriteStream(outputFilename);
-  let firstLineWritten = false;
+const saveToCSV = (data, filename) => {
+  const headers = Object.keys(data[0]);
 
-  return new Writable({
-    objectMode: true,
-    write(chunk, encoding, callback) {
-      const row = Object.values(chunk).join(",") + "\n";
+  const csvData = [
+    headers.join(","),
+    ...data.map((row) => headers.map((header) => row[header]).join(",")),
+  ].join("\n");
 
-      if (!firstLineWritten) {
-        const headers = Object.keys(chunk).join(",");
-        writableStream.write(headers + "\n");
-        firstLineWritten = true;
-      }
-
-      writableStream.write(row, encoding, callback);
-    },
+  fs.writeFile(filename, csvData, (err) => {
+    if (err) {
+      console.error("Erro ao salvar o arquivo CSV:", err);
+    } else {
+      console.log("Arquivo CSV salvo com sucesso!");
+    }
   });
 };
 
-const countTotalLines = (inputFilename) => {
-  return new Promise((resolve, reject) => {
-    let lineCount = 0;
-
-    fs.createReadStream(inputFilename)
-      .pipe(csvParser())
-      .on("data", () => {
-        lineCount++;
-      })
-      .on("end", () => {
-        resolve(lineCount);
-      })
-      .on("error", (err) => {
-        reject(err);
-      });
-  });
-};
-
-const processCSV = async (inputFilename, outputFilename) => {
-  const totalLines = await countTotalLines(inputFilename);
-  console.log(`Total de linhas a serem processadas: ${totalLines}`);
-
-  const writableStream = saveToCSVStream(outputFilename);
-
-  const startTime = Date.now();
-  let processedLines = 0;
-
-  const displayProgress = () => {
-    const elapsedTime = (Date.now() - startTime) / 1000;
-    const estimatedTimeRemaining =
-      (elapsedTime / processedLines) * (totalLines - processedLines);
-
-    console.clear();
-    console.log(`Linhas processadas: ${processedLines} / ${totalLines}`);
-    console.log(`Tempo decorrido: ${elapsedTime.toFixed(2)} segundos`);
-    console.log(
-      `Estimativa de tempo restante: ${estimatedTimeRemaining.toFixed(
-        2
-      )} segundos`
-    );
-  };
+const processCSV = (inputFilename, outputFilename) => {
+  const results = [];
 
   fs.createReadStream(inputFilename)
     .pipe(csvParser())
     .on("data", (row) => {
       const transformedRow = transformResult(row);
-      writableStream.write(transformedRow);
 
-      processedLines++;
-
-      if (processedLines % 10 === 0) {
-        setTimeout(displayProgress, 3000);
-      }
+      results.push(transformedRow);
     })
     .on("end", () => {
-      writableStream.end();
-      console.log("Arquivo CSV processado e salvo com sucesso!");
-    })
-    .on("error", (err) => {
-      console.error("Erro ao processar o CSV:", err);
+      saveToCSV(results, outputFilename);
     });
 };
 
-processCSV("data.csv", "output.csv");
+processCSV("dataWithValidData.csv", "output.csv");
